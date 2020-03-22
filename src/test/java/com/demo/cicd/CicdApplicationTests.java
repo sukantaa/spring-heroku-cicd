b7342c2f225cfeb727ebb83fb0a7aa52162c002a
Sukanta <sukantaban1993@gmail.com>
2020-03-22 13:30:42 +0530

    Added Test case

--- src/test/java/com/demo/cicd/CicdApplicationTests.java
+++ src/test/java/com/demo/cicd/CicdApplicationTests.java
@@ -1,13 +1,24 @@
 package com.demo.cicd;
 
+import org.junit.Assert;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+import com.demo.cicd.controller.PingController;
+
+@RunWith(SpringRunner.class)
 @SpringBootTest
 class CicdApplicationTests {
 
+	@Autowired
+	private PingController pingController;
+
 	@Test
-	void contextLoads() {
+	public void contextLoads() {
+		Assert.assertEquals("Pinging ....",pingController.home() );
 	}
 
 }
